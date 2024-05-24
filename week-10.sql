db2686103236c9c7482a635423898dfa23e8b9c0
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-05-24 11:24:46 -0500

    Create week-10.sql

---
+++ week-10.sql
@@ -0,0 +1,32 @@
+/*
+Ejercicio 01:
+Crear una función que retorne la cantidad total de pedidos
+*/
+
+/*
+Ejercicio 02:
+Crear una función que retorne la cantidad de pedidos realizados para un determinado año
+ingresado como parámetro.
+*/
+
+/*
+Ejercicio 03:
+Crear una función que retorne la cantidad de artículos vendidos en un determinado año.
+*/
+
+/*
+Ejercicio 04:
+Crear una función donde ingrese el nombre del país destinatario del pedido y
+retorne el total de las unidades vendidas.
+*/
+
+/*
+Ejercicio 05:
+Crear una función que retorne los clientes que realizaron pedidos en un determinado año
+ingresado como parámetro. Para cada cliente debe mostrar el código y nombre.
+*/
+
+/*
+Ejercicio 06:
+Crear un procedimiento almacenado que permitar registrar los datos de un cliente.
+*/
