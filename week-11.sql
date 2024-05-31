4682c2cc4969615a327e0a2f591c4b3e16248c79
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-05-31 13:11:21 -0500

    Update week-11.sql

--- week-11.sql
+++ week-11.sql
@@ -49,3 +49,89 @@ select CategoryName from VItemsQuantityByCategoryByYear
 where year =@Year  and Total = (select max(Total) from VItemsQuantityByCategoryByYear
 								where Year = @Year)
 go
+
+/*
+Implemente un procedimiento almacenado que imprima cada uno de los registros de los productos indicando su nombre y unidades en stock, y al finalizar, imprima el total del inventario.
+*/
+create procedure SPInventory 
+as
+begin
+	/* Paso 1: declarar el cursor */
+	declare ProductsCursor cursor for
+	select ProductName, UnitsInStock from Products
+
+	/* Paso 2: abrir el cursor */
+	open ProductsCursor
+
+	/* Paso 3: declarar variables para cada uno de los datos de la consulta */
+	declare @ProductName nvarchar(40)
+	declare @Stock smallint
+
+	declare @Total smallint
+	set @Total = 0
+
+	/* Paso 4: leer una fila */
+	fetch ProductsCursor into @ProductName, @Stock
+
+	while(@@fetch_status = 0)
+	begin
+		print @ProductName + ': ' + cast (@Stock as nvarchar(6))
+		set @Total = @Total + @Stock
+		fetch ProductsCursor into @ProductName, @Stock
+	end
+
+	print 'Inventario: ' + cast (@Total as nvarchar(6))
+
+	/* Paso 5: cerrar el cursor */ 
+	close ProductsCursor
+
+	/* Paso 6: liberar los recursos utilizador por el cursor*/
+	deallocate ProductsCursor
+end
+go
+
+exec SPInventory
+go
+
+/*
+Crear un procedimiento almacenado para insertar un nuevo cliente
+*/
+
+create procedure SPInsertCustomer
+	@CustomerID nchar(5),
+	@CompanyName nvarchar(40)
+as
+begin
+	begin try
+		begin transaction TInsert
+			insert into Customers(CustomerID, CompanyName)
+			values (@CustomerID, @CompanyName)
+			print 'Se registró el cliente'
+		commit
+	end try
+	begin catch
+		if (@@TRANCOUNT > 0)
+		begin
+			print error_message()
+			rollback
+		end
+	end catch	
+end
+go
+
+/*
+Crear un trigger para la operación delete de la tabla Customers
+*/
+create trigger TRICustomers on Customers
+for delete
+as
+begin
+	select * from deleted
+	print 'Se eliminó un cliente'
+end
+go
+
+delete from Customers where CustomerID = 'BBBBB'
+go
+
+
